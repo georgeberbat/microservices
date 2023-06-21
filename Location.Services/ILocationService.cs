34391c746243274a90d5167420b551ec72e8f4ab
Бербат Георгий <g.berbat@dextechnology.com>
2023-06-21 22:03:41 +0300

    LP feat: add location contract

---
+++ Location.Services/ILocationService.cs
@@ -0,0 +1,13 @@
+﻿namespace Location.Services;
+
+public interface ILocationService
+{
+    IEnumerable<Models.Location> Get();
+    IEnumerable<Models.Location> SearchByName(string substring);
+    IEnumerable<Models.Location> SearchByAddress(string substring);
+    IEnumerable<Models.Location> SearchByArea(double minLatitude, double maxLatitude, double minLongitude, double maxLongitude);
+    
+    void Create(Models.Location location);
+    void Update(Models.Location location);
+    void Delete(Guid id);
+}
